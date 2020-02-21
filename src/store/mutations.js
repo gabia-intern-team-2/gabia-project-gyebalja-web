49fca5fd03a240a5610630a3dacb3fea4ab1e616
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-02-21 14:26:41 +0900

    #15 Vuex-게시판 게시글 리스트, 교육 리스트 조회 (#16)
    #15 Vuex-게시판
    
    - 게시글 리스트 조회
    
    - 교육 리스트 조회

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,10 @@
 // https://vuex.vuejs.org/en/mutations.html
 
 export default {
-  //
+  SET_BOARDS (state, boards) {
+    state.boards = boards
+  },
+  SET_EDUCATIONS (state, educations) {
+    state.educations = educations
+  }
 }
