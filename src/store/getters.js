877dce794367c9506211653b46207044f0a54e88
LEEHYUNJAE <hjjae2@gmail.com>
2020-02-21 12:55:08 +0900

    #15 Vuex-게시판 게시글 리스트, 교육 리스트 조회

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,11 @@
 // https://vuex.vuejs.org/en/getters.html
 
 export default {
-  //
+  fetchedBoards (state) {
+    console.log(state.boards.response)
+    return state.boards.response
+  },
+  fetchedEducations (state) {
+    return state.educations.response
+  }
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
 // https://vuex.vuejs.org/en/state.html
 
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
 export default {
-  //
+  state: {
+    boards: [],
+    educations: []
+  }
 }
