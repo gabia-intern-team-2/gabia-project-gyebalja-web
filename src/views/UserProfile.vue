a900d35f3758f9e0ad9b197f32586540c515ac51
TaeGyun-Jung <wjdxorbs010@gmail.com>
2020-02-24 18:08:03 +0900

    #41 Layout - 사용자 정보 관리 작성

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -12,131 +12,137 @@
         md8
       >
         <material-card
-          color="green"
-          title="Edit Profile"
-          text="Complete your profile"
+          color="purple"
+          title="내 정보 상세"
+          text="your profile"
         >
-          <v-form>
-            <v-container py-0>
-              <v-layout wrap>
-                <v-flex
-                  xs12
-                  md4
-                >
-                  <v-text-field
-                    label="Company (disabled)"
-                    disabled/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4
-                >
-                  <v-text-field
-                    class="purple-input"
-                    label="User Name"
-                  />
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4
-                >
-                  <v-text-field
-                    label="Email Address"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md6
-                >
-                  <v-text-field
-                    label="First Name"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md6
-                >
-                  <v-text-field
-                    label="Last Name"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md12
-                >
-                  <v-text-field
-                    label="Adress"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4>
-                  <v-text-field
-                    label="City"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4>
-                  <v-text-field
-                    label="Country"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4>
-                  <v-text-field
-                    class="purple-input"
-                    label="Postal Code"
-                    type="number"/>
-                </v-flex>
-                <v-flex xs12>
-                  <v-textarea
-                    class="purple-input"
-                    label="About Me"
-                    value="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                  />
-                </v-flex>
-                <v-flex
-                  xs12
-                  text-xs-right
-                >
-                  <v-btn
-                    class="mx-0 font-weight-light"
-                    color="success"
-                  >
-                    Update Profile
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-form>
-        </material-card>
-      </v-flex>
-      <v-flex
-        xs12
-        md4
-      >
-        <material-card class="v-card-profile">
-          <v-avatar
-            slot="offset"
-            class="mx-auto d-block"
-            size="130"
+          <v-flex
+            xs12
+            md12
           >
-            <img
-              src="https://demos.creative-tim.com/vue-material-dashboard/img/marc.aba54d65.jpg"
-            >
-          </v-avatar>
-          <v-card-text class="text-xs-center">
-            <h6 class="category text-gray font-weight-thin mb-3">CEO / CO-FOUNDER</h6>
-            <h4 class="card-title font-weight-light">Alec Thompson</h4>
-            <p class="card-description font-weight-light">Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...</p>
+            <material-card class="v-card-profile">
+              <v-avatar
+                slot="offset"
+                class="mx-auto d-block"
+                size="200"
+              >
+                <img
+                  src="../images/dog.jpg"
+                >
+              </v-avatar>
+              <v-card-text class="text-xs-center">
+                <h4 class="category text-gray font-weight-thin mb-3">인턴 / HR부서</h4>
+                <h3 class="card-title font-weight-light"><v-icon color="black">mdi-account</v-icon>정태균</h3>
+              </v-card-text>
+              <v-container py-0>
+                <v-layout wrap>
+                  <v-flex
+                    xs12
+                    md7
+                  >
+                    <v-card
+                      class="mx-auto font-weight-bold"
+                      max-width="800px"
+                      hover
+                    >
+                      <v-card-text>
+                        <h6 class="grey--text font-italic"><v-icon color="grey">mdi-email</v-icon>이메일(ID)</h6>
+                        <div>test@gabia.com</div>
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    md3
+                  >
+                    <v-card
+                      class="mx-auto  text-xs-center"
+                      max-width="200px"
+                      hover
+                    >
+                      <v-card-text>
+                        <h6 class="grey--text font-italic text-xs-left"><v-icon color="grey">mdi-gender-male-female</v-icon>성별(Gender)</h6>
+                        <div>MALE</div>
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    md2
+                  >
+                    <v-card
+                      class="mx-auto text-xs-center"
+                      max-width="200px"
+                      hover
+                      dark
+                    >
+                      <v-card-text>
+                        <h6 class="grey--text font-italic text-xs-left"><v-icon color="grey">mdi-shield</v-icon>등록번호</h6>
+                        <div>No.200</div>
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    md6
+                  >
+                    <v-card
+                      class="mx-auto"
+                      min-width="300px"
+                      hover
+                    >
+                      <v-card-text>
+                        <div>
+                          <h6 class="grey--text font-italic"><v-icon color="grey">mdi-phone</v-icon>핸드폰 번호(Phone Number)</h6>
+                          010-9999-9999
+                        </div>
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    md6
+                  >
+                    <v-card
+                      class="mx-auto"
+                      min-width="300px"
+                      hover
+                    >
+                      <v-card-text>
+                        <h6 class="grey--text font-italic"><v-icon color="grey">mdi-phone-classic</v-icon>내선 번호(Tel.)</h6>
+                        <div>
+                          031-2222-2222
+                        </div>
+                      </v-card-text>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </material-card>
+          </v-flex>
+          <v-flex
+            xs12
+            text-xs-right
+          >
+            <router-link
+              :to="{name: 'Profile Edit'}">
+              <v-btn
+                class="mx-1 font-weight-light white--text"
+                color="teal"
+              >
+                <v-icon color="white">mdi-pen</v-icon>
+                수정
+              </v-btn>
+            </router-link>
             <v-btn
-              color="success"
-              round
-              class="font-weight-light"
-            >Follow</v-btn>
-          </v-card-text>
+              class="mx-1 font-weight-light"
+              color="info"
+              @click="home"
+            >
+              <v-icon>mdi-home</v-icon>
+              홈으로
+            </v-btn>
+          </v-flex>
         </material-card>
       </v-flex>
     </v-layout>
@@ -145,6 +151,15 @@
 
 <script>
 export default {
-  //
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    home () {
+      this.$router.push({ name: 'DashBoard' })
+    }
+  }
 }
 </script>
