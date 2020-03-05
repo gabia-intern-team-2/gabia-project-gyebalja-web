72f5a6fcc99d2c32756588235c1ebadfd457d0cb
LEEHYUNJAE <hjjae2@gmail.com>
2020-03-05 11:16:43 +0900

    #86 withCredential 중복 제거

--- src/api/department/department.js
+++ src/api/department/department.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import { config } from '../index.js'
 
+axios.defaults.withCredentials = true
+
 /** 부서 API */
 // 조회 - 부서 (전체)
 function getDepartmentList () {
-  return axios.get(`${config.baseUrl}departments`, { withCredentials: true })
+  return axios.get(`${config.baseUrl}departments`)
 }
 
 export {
