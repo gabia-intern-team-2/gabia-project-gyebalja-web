e74186c2a281ceef2dfa5220fcb4834a65acdd36
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-03-06 10:12:56 +0900

    #86 Layout - 게시글 에디터 구현 (#89)
    * #86 withCredential 중복 제거
    
    * #86 호스트 명 변경, gyebalja.com
    
    * #86 호스트 명 변경, api.gyeblja.com
    
    * #86 vue.config 파일, api/index.js, BoardImg 등록 api 수정
    
    * #86 불필요한 부분, 주석 삭제

--- src/api/board/board.js
+++ src/api/board/board.js
@@ -1,30 +1,32 @@
 import axios from 'axios'
 import { config } from '../index.js'
 
+axios.defaults.withCredentials = true
+
 /** 게시글 API */
 // 생성 - 게시글 (한 개)
 function postBoardItem (board) {
-  return axios.post(`${config.baseUrl}boards`, board, { withCredentials: true })
+  return axios.post(`${config.baseUrl}boards`, board)
 }
 
 // 조회 - 게시글 (한 개)
 function getBoardItem (boardId) {
-  return axios.get(`${config.baseUrl}boards/${boardId}`, { withCredentials: true })
+  return axios.get(`${config.baseUrl}boards/${boardId}`)
 }
 
 // 조회 - 게시글 (전체)
 function getBoardList () {
-  return axios.get(`${config.baseUrl}boards`, { withCredentials: true })
+  return axios.get(`${config.baseUrl}boards`)
 }
 
 // 수정 - 게시글 (한 개)
 function putBoardItem (boardId, board) {
-  return axios.put(`${config.baseUrl}boards/${boardId}`, board, { withCredentials: true })
+  return axios.put(`${config.baseUrl}boards/${boardId}`, board)
 }
 
 // 삭제 - 게시글 (한 개)
 function deleteBoardItem (boardId) {
-  return axios.delete(`${config.baseUrl}boards/${boardId}`, { withCredentials: true })
+  return axios.delete(`${config.baseUrl}boards/${boardId}`)
 }
 
 export {
