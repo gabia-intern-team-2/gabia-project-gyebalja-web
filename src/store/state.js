d4f001fa6941e2e9cd3369e396ace5205f37adf0
LEEHYUNJAE <35790290+hjjae2@users.noreply.github.com>
2020-03-09 10:08:48 +0900

    #94 Layout - Dashboard (StatisticsEvent.js 파일 삭제, 레이아웃 수정) (#95) (#96)
    * #94 통계 Event 파일 삭제 -> vuex로 변환 작업 필요
    
    * #94 chartist no data message 추가
    
    * #94 메인화면 css 수정, 통계 option max 값 설정
    
    * #94 게시판 페이징 개선
    
    * #94 v-data-table 헤더 -> vuex 관리
    
    * #94 v-data-table 헤더 -> vuex 관리

--- src/store/state.js
+++ src/store/state.js
@@ -9,6 +9,66 @@ export default {
   state: {
     boards: [],
     educations: [],
-    departments: []
+    departments: [],
+    boardHeaders: [
+      {
+        sortable: false,
+        text: 'No',
+        value: Number
+      },
+      {
+        sortable: false,
+        text: '제목',
+        value: 'country'
+      },
+      {
+        sortable: false,
+        text: '조회수',
+        value: 'views'
+      },
+      {
+        sortable: false,
+        text: '수정일',
+        value: 'modifiedDate'
+      }
+    ],
+    educationHeaders: [
+      {
+        sortable: false,
+        text: '교육명',
+        value: 'title',
+        width: '480px'
+      },
+      {
+        sortable: false,
+        text: '시작날짜',
+        value: 'startDate'
+      },
+      {
+        sortable: false,
+        text: '종료날짜',
+        value: 'endDate'
+      },
+      {
+        sortable: false,
+        text: '시간',
+        value: 'totalHours'
+      },
+      {
+        sortable: false,
+        text: '교육유형',
+        value: 'type'
+      },
+      {
+        sortable: false,
+        text: '장소',
+        value: 'place'
+      },
+      {
+        sortable: false,
+        text: '카테고리',
+        value: 'category'
+      }
+    ]
   }
 }
